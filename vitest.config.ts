b7b69e34ec3a0c9afb347d047519150ce0f524af
Rohith Poyyeri <rohithart@yahoo.com>
2025-07-15 00:17:47 +1000

    fix: Test reports

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,8 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'node',
-    reporters: ['json', 'default'],
-      outputFile: './test-output.json',
+    reporters: ['default', ['junit', { outputFile: 'reports/test-results.xml' }]],
     coverage: {
       provider: 'istanbul',
     },

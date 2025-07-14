d7ed689b80f4d32c77000163dcb74acc77400a17
Rohith Poyyeri <rohithart@yahoo.com>
2025-07-14 19:11:20 +1000

    feat: Test setup

---
+++ jest.config.ts
@@ -0,0 +1,30 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest/presets/js-with-ts-esm',  // ESM bridge
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { useESM: true }]
+  },
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  testMatch: ['**/*.spec.ts'],
+  coverageThreshold: {
+    global: {
+      statements: 100,
+      branches: 75,
+      functions: 100,
+      lines: 100,
+    },
+  },
+  reporters: [
+  'default',
+  ['jest-junit', { outputDirectory: 'reports', outputName: 'test-results.xml' }]
+],
+
+};
+
+export default config;

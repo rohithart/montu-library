1dc1661f0055d3568a9db9129c91dfd15354d24e
Rohith Poyyeri <rohithart@yahoo.com>
2025-07-14 23:37:11 +1000

    fix: Update config

--- jest.config.ts
+++ jest.config.ts
@@ -7,24 +7,29 @@ const config: Config = {
   transform: {
     '^.+\\.tsx?$': ['ts-jest', { useESM: true }]
   },
+  testEnvironmentOptions: {
+    node: 'current'
+  },
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov'],
   moduleFileExtensions: ['ts', 'js', 'json'],
   testMatch: ['**/*.spec.ts'],
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1'
+  },
   coverageThreshold: {
     global: {
       statements: 100,
-      branches: 75,
+      branches: 80,
       functions: 100,
-      lines: 100,
-    },
+      lines: 100
+    }
   },
   reporters: [
-  'default',
-  ['jest-junit', { outputDirectory: 'reports', outputName: 'test-results.xml' }]
-],
-
+    'default',
+    ['jest-junit', { outputDirectory: 'reports', outputName: 'test-results.xml' }]
+  ]
 };
 
 export default config;

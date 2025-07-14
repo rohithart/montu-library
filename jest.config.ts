34458c3a9e22938e9898bfdacfae67c3e9de9fe9
Rohith Poyyeri <rohithart@yahoo.com>
2025-07-14 23:26:37 +1000

    fix: Jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,23 @@
 import type { Config } from 'jest';
+import { TS_EXT_TO_TREAT_AS_ESM, ESM_TS_TRANSFORM_PATTERN } from 'ts-jest'
 
 const config: Config = {
   preset: 'ts-jest/presets/js-with-ts-esm',  // ESM bridge
   testEnvironment: 'node',
-  extensionsToTreatAsEsm: ['.ts'],
+  // extensionsToTreatAsEsm: ['.ts'],
+  // transform: {
+  //   '^.+\\.tsx?$': ['ts-jest', { useESM: true }]
+  // },
+
+  extensionsToTreatAsEsm: [...TS_EXT_TO_TREAT_AS_ESM],
   transform: {
-    '^.+\\.tsx?$': ['ts-jest', { useESM: true }]
+    [ESM_TS_TRANSFORM_PATTERN]: [
+      'ts-jest',
+      {
+        //...other `ts-jest` options
+        useESM: true,
+      },
+    ],
   },
   collectCoverage: true,
   coverageDirectory: 'coverage',
